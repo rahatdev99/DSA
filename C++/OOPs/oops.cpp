8b8c75c7b8cd27a5816a383ef8ec9b1060dbce70
rahatdev99 <rahatdev99@gmail.com>
2022-11-23 13:22:56 +0600

    oops

--- C++/OOPs/oops.cpp
+++ C++/OOPs/oops.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
+
 using namespace std;
+
+class Hero {
+    int health;
+    char level;
+};
+
 
 int main()
 {
-    
+
+    Hero h1;
+
+    cout<<sizeof(h1)<<endl;
+
     return 0;
 }
