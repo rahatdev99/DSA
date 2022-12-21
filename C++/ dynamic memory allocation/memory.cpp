079e6e876f055a957040a2135ace56a4d09b9ea2
rahatdev99 <rahatdev99@gmail.com>
2022-12-21 08:56:52 +0600

    string

--- C++/ dynamic memory allocation/memory.cpp
+++ C++/ dynamic memory allocation/memory.cpp
@@ -1,8 +1,38 @@
 #include <iostream>
 using namespace std;
 
+#define PI 3.14
+
+void print(int *arr,int n) {
+    for(int i = 0; i < n; i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
 int main()
 {
+    // int n;
+    // cin>>n;
     
+    // int *arr = new int[n];
+
+    // for(int i = 0; i < n; i++) {
+    //     cin>>arr[i];
+    // }
+
+
+    // print(arr,n);
+
+    int r = 5;
+
+    double area = PI * r * r;
+
+    cout<<area<<endl;
+
+
+    
+
     return 0;
 }
